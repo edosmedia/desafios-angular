846828b82af498e04066ef83e6069ffad087d3bc
Eduardo Medrano <eduardomedranop@gmail.com>
2022-12-07 00:33:56 -0300

    Add Validators v1

--- src/app/clase6/formularios/formularios.component.ts
+++ src/app/clase6/formularios/formularios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formularios',
@@ -10,16 +10,23 @@ export class FormulariosComponent implements OnInit {
   public formularioPrincipal: FormGroup;
   public formularioSegundario: FormGroup;
 
-  constructor(
-    private fb: FormBuilder
-    ) { }
+  constructor(private fb: FormBuilder ) { }
 
   ngOnInit(): void {
     this.formularioPrincipal = this.fb.group({
-      nombre: ['Eduardo'],
-      apellido: ['Medrano Piedra'],
-      email: ['ejemplo@gmail.com'],
-      password: ['123']
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', Validators.required, Validators.minLength(3)],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.pattern(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          ),
+        ],
+      ],
+      password: ['', Validators.required, Validators.minLength(1)],
     });
     this.formularioSegundario;
   }
