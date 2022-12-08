111413273cfef028faf7e1fcedb2fe412ab121e0
Eduardo Medrano <eduardomedranop@gmail.com>
2022-12-08 20:44:45 -0300

    desafio clase 6 terminado corregido errores

--- src/app/clase6/formularios/formularios.component.ts
+++ src/app/clase6/formularios/formularios.component.ts
@@ -14,19 +14,36 @@ export class FormulariosComponent implements OnInit {
 
   ngOnInit(): void {
     this.formularioPrincipal = this.fb.group({
-      nombre: ['', [Validators.required, Validators.minLength(3)]],
-      apellido: ['', Validators.required, Validators.minLength(3)],
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(72),
+        ],
+      ],
+      apellido: [
+        '',
+        [Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(72)]
+      ],
       email: [
         '',
         [
           Validators.required,
-          Validators.minLength(20),
+          Validators.minLength(10),
           Validators.pattern(
             /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           ),
         ],
       ],
-      password: ['', Validators.required, Validators.minLength(1)],
+      pass: [
+        '',
+        [Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(20),]
+      ],
     });
     this.formularioSegundario;
   }
